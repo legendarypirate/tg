aefd57326cb59b6c93b55d40ba6d98b6c8d8bba2
legendarypirate <33424038+legendarypirate@users.noreply.github.com>
2021-11-15 09:08:37 +0800

    sssdd
    dddd

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -135,31 +135,7 @@
     
                                 </div>
                             </div>
-                            <div class="carousel-item">
-                                <div class="row">
-    
-                                    <div class="col-md-6 mb-3">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <h4 class="card-title">Special title treatment</h4>
-                                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    
-                                            </div>
-    
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <h4 class="card-title">Special title treatment</h4>
-                                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    
-                                            </div>
-                                        </div>
-                                    </div>
-                                 
-                                </div>
-                            </div>
+                          
                         </div>
                     </div>
                 </div>
